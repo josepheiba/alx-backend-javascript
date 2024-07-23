8da4299ff2b92f54750c55ec77b9121dbc6142c2
Joseph Eiba <josepheiba@icloud.com>
2024-07-23 01:23:10 +0100

    commit db

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,12 +4,11 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const year = getCurrentYear();
-  const budget = {};
-
-  budget[`income-${year}`] = income;
-  budget[`gdp-${year}`] = gdp;
-  budget[`capita-${year}`] = capita;
+  const budget = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
 
   return budget;
 }
