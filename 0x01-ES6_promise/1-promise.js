763fd88f9eff28c0414ac643087c4fc013ee3ae1
Joseph Eiba <josepheiba@icloud.com>
2024-07-24 23:33:26 +0100

    commit db

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,17 +1,15 @@
 /* eslint-disable no-unused-vars */
-/* eslint-disable prefer-promise-reject-errors */ // Disable specific ESLint rule for rejection errors
+/* eslint-disable prefer-promise-reject-errors */
 
-const getFullResponseFromAPI = (success) => {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    } else {
-      reject(new Error('The fake API is not working currently')); // Ensure rejection reason is an Error object
-    }
-  });
-};
+const getFullResponseFromAPI = (success) => new Promise((resolve, reject) => {
+  if (success) {
+    resolve({
+      status: 200,
+      body: 'Success',
+    });
+  } else {
+    reject(new Error('The fake API is not working currently'));
+  }
+});
 
 export default getFullResponseFromAPI;
