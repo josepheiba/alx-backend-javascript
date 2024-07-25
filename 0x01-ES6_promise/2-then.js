07cbddfa8118a9d75c999d66b7a8b00605862f97
Joseph Eiba <josepheiba@icloud.com>
2024-07-25 01:05:50 +0100

    commit db

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable prefer-promise-reject-errors */
 
-const handleResponseFromAPI = (promise) => promise.then(() => ({
-  status: 200,
-  body: 'Success',
-})).catch(() => Error()).finally(() => {
-  console.log('Got a response from the API');
-});
+const handleResponseFromAPI = (promise) => promise
+  .then(() => ({
+    status: 200,
+    body: 'Success',
+  }))
+  .catch(() => Error())
+  .finally(() => {
+    console.log('Got a response from the API');
+  });
 
 export default handleResponseFromAPI;
