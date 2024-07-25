90193869adc302b57829c9cbb367acdfff8693f6
Joseph Eiba <josepheiba@icloud.com>
2024-07-25 01:18:50 +0100

    commit db

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,8 @@
-const handleResponseFromAPI = (promise) => {
-  promise.then(() => ({
-    status: 200,
-    body: 'Success',
-  }))
-    .catch(() => Error())
-    .finally(() => console.log('Got a response from the API'));
-};
+const handleResponseFromAPI = (promise) => promise.then(() => ({
+  status: 200,
+  body: 'Success',
+}))
+  .catch(() => Error())
+  .finally(() => console.log('Got a response from the API'));
 
 export default handleResponseFromAPI;
